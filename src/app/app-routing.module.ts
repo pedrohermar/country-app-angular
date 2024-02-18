9cd2326c87321e508486ab1fe5cef1b95e0acf02
Pedro <pedrohm92@gmail.com>
2024-02-18 14:34:02 +0100

    Configuradas routas principales y con lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './shared/pages/homePage/home-page.component';
+
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent
   },
   {
@@ -13,8 +15,16 @@ const routes: Routes = [
     component: AboutPageComponent
   },
   {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule)
+  },
+  {
     path: '**',
-    redirectTo: 'home'
+    redirectTo: ''
   },
 ]
 

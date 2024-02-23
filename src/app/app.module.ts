24ce7c2ba01b4429092aaea7f430cc5999ec06d2
Pedro <pedrohm92@gmail.com>
2024-02-23 22:30:40 +0100

    Creación Counties Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -12,7 +13,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     AppRoutingModule,
     BrowserModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+  private apiUrl: string = "https://restcountries.com/v3.1"
+
+  constructor(private http: HttpClient) { }
+
+  searchCapital(query: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${query}`
+    return this.http.get<Country[]>(url)
+  }
+}

f2fdc518ffb572b5b61a32bd9b3b5eba23b9e222
Pedro <pedrohm92@gmail.com>
2024-03-02 23:55:53 +0100

    Configurada página de datos del país

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styles: ``
 })
 export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -22,12 +25,8 @@ export class CountryPageComponent implements OnInit {
         switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id))
       )
       .subscribe(country => {
-        if (!country) {
-          return this.router.navigateByUrl('')
-        }
-
-        console.log(country)
-        return
+        if (!country) return this.router.navigateByUrl('');
+        return this.country = country;
       })
   }
 

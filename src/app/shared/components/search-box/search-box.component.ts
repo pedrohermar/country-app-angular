9ee026c630ce189ab69e74edb875c9687c1f0111
Pedro <pedrohm92@gmail.com>
2024-03-10 16:52:09 +0100

    Implementación de un debounce en searchbox

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,10 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime, pipe } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  private debouncer: Subject<string> = new Subject<string>()
 
   @Input()
   placeholder: string = "";
@@ -12,7 +15,24 @@ export class SearchBoxComponent {
   @Output()
   onValue = new EventEmitter<string>();
 
+  @Output()
+  onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(
+        debounceTime(500)
+      )
+      .subscribe(value => {
+        this.onDebounce.emit(value)
+      })
+  }
+
   emitValue(value: string): void {
-    this.onValue.emit(value);
+    this.onValue.emit(value)
   };
+
+  onKeyPress(query: string) {
+    this.debouncer.next(query)
+  }
 }

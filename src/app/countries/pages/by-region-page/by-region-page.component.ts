87ccecb8ddc2f428fec4e84afba9215ecda097be
Pedro <pedrohm92@gmail.com>
2024-03-17 23:40:46 +0100

    Busqueda por region cambiada a botones de seleción

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania'
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,14 +13,18 @@ export class ByRegionPageComponent {
 
   constructor(private countriesService: CountriesService) { }
 
-  countries: Country[] = []
-  public isLoading: boolean = false
+  public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
+  public isLoading: boolean = false;
 
-  searchByRegion(value: string) {
+  searchByRegion(region: Region) {
+
+    this.selectedRegion = region;
 
     this.isLoading = true
 
-    this.countriesService.searchRegion(value)
+    this.countriesService.searchRegion(region)
       .subscribe(countries => {
         this.countries = countries
         this.isLoading = false
